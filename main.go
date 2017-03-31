12d3ed0d78e81572fec970e1be744f19b3dc7b7b
Dmitriy Fedorenko <df@wheely.com>
2017-03-31 19:26:17 +0300

    Simple version

--- main.go
+++ main.go
@@ -1,6 +1,67 @@
 package main
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/api/types"
+)
 
 func main() {
+	cli, err := client.NewEnvClient()
+	if nil != err {
+		panic(err)
+	}
 
+	containers, err := cli.ContainerList(
+		context.Background(),
+		types.ContainerListOptions{All: true},
+	)
+	if nil != err {
+		panic(err)
+	}
+
+	fmt.Printf("Containers count %d\n", len(containers))
+
+
+	images, err := cli.ImageList(
+		context.Background(),
+		types.ImageListOptions{All: true},
+	)
+	if nil != err {
+		panic(err)
+	}
+
+	fmt.Printf("Images count %d\n", len(images))
+
+
+	uselessImageIds := []string{}
+
+	for _, image := range images {
+		imageUsed := false
+		for _, container := range containers {
+			if container.ImageID == image.ID {
+				imageUsed = true
+				break
+			}
+		}
+
+		if !imageUsed {
+			uselessImageIds = append(uselessImageIds, image.ID)
+		}
+	}
+
+	for _, imageID := range uselessImageIds {
+		response, err := cli.ImageRemove(
+			context.Background(),
+			imageID,
+			types.ImageRemoveOptions{},
+		)
+		if nil != err {
+			fmt.Printf("Err: %s\n", err)
+		}
+
+		fmt.Printf("Response: %+v\n", response)
+	}
 }
